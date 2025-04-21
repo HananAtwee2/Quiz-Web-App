e163c6109f3571bb6f177ed801f7910011b6207c
Your Name <atwihanan20@gmail.com>
2025-04-21 22:13:31 +0300

    created 3 quizes and saving them

--- scripts/home.js
+++ scripts/home.js
@@ -6,3 +6,11 @@ if (!currentUser) {
 // Show welcome message
 document.getElementById("welcome-user").textContent = `Hello, ${currentUser.username}! Ready to test your knowledge?`;
 
+const quizzes = JSON.parse(localStorage.getItem("quizzes")) || [
+    { id: 1, title: "JavaScript Basics" },
+    { id: 2, title: "HTML & CSS" },
+    { id: 3, title: "General Knowledge" }
+];
+
+//save to localstorage if not saved yet
+localStorage.setItem("quizzes", JSON.stringify(quizzes));
